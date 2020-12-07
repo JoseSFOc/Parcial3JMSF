9cb387c1d26c25ae51c4c5eedb7c87c4b61b850b
JoseSF <0619716120@uma.es>
2020-12-07 16:49:12 +0100

    adios

--- backend/api/routes/airQuality.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const airQualityService = require("../services/airQualityService");
-
-router.get("/", airQualityService.findAll);
-
-module.exports = router;
