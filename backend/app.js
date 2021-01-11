2185fb895d606ba3f08a9437a6b729adb287f17f
JoseSF <0619716120@uma.es>
2021-01-11 15:40:20 +0100

    another one

--- backend/app.js
+++ backend/app.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization, access-control-allow-origin"
   );
   res.header(
     "Access-Control-Request-Headers",
