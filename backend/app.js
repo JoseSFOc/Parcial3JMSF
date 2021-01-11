3b73eec215ce3cd236ba712d503fda038c21be7c
JoseSF <0619716120@uma.es>
2021-01-11 16:06:13 +0100

    local

--- backend/app.js
+++ backend/app.js
@@ -23,7 +23,7 @@ app.use(
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(cors({ origin: "null" }));
+app.use(cors({ origin: "http://localhost:3000" }));
 /*app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
