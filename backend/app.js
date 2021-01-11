ea75074e9b927dfcecbef7e6fe6bfcd2d1b1f5fc
JoseSF <0619716120@uma.es>
2021-01-11 15:59:56 +0100

    3000

--- backend/app.js
+++ backend/app.js
@@ -43,7 +43,7 @@ app.use(cors());
 // Add headers
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:8888");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
 
   // Request methods you wish to allow
   res.setHeader(
