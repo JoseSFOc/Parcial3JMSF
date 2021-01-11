abc03752c28f04111733975df358a6fbc0ca4594
JoseSF <0619716120@uma.es>
2021-01-11 15:38:12 +0100

    added more headers

--- backend/app.js
+++ backend/app.js
@@ -28,6 +28,10 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
+  res.header(
+    "Access-Control-Request-Headers",
+    "X-Requested-With, accept, content-type"
+  );
   if (req.method === "OPTIONS") {
     res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
     return res.status(200).json({});
