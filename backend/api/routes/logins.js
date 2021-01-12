9694b0a87c5632049b7bdb6be7184c4ae314b537
JoseSF <0619716120@uma.es>
2021-01-12 13:23:55 +0100

    login route

--- backend/api/routes/logins.js
+++ backend/api/routes/logins.js
@@ -2,6 +2,12 @@ const express = require("express");
 const router = express.Router();
 const loginService = require("../services/loginService");
 
+router.options("/", function (req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  res.end();
+});
 router.post("/", loginService.login);
 
 module.exports = router;
