1761ce909b01de4927a2093bba46ab0ac8bd72cc
JoseSF <0619716120@uma.es>
2021-01-15 19:38:04 +0100

    front

--- frontend/src/components/image/Images.js
+++ frontend/src/components/image/Images.js
@@ -1,15 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { Card } from "react-bootstrap";
+import { Card, Button } from "react-bootstrap";
 
-const Images = ({ _id, description, imageUrl, hashtags, author, likes }) => {
+const url = "https://parcial3-jmsf.herokuapp.com/images/";
+
+const Images = (image) => {
+  const { _id, description, imageUrl, hashtags, author, likes } = image;
+  const [imageSet, setImageSet] = useState(image);
+
+  const updateImage = async (image) => {
+    await fetch(url + image._id, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(image),
+      redirect: "follow",
+    });
+    setImageSet(image);
+  };
+
   return (
     <Card style={{ width: "18rem" }}>
       {imageUrl ? <Card.Img variant="top" src={imageUrl} /> : <></>}
       <Card.Body>
         <Card.Title>By {author.name}</Card.Title>
-        <Card.Subtitle>Likes {likes}</Card.Subtitle>
+        <Card.Subtitle>Likes {imageSet.likes}</Card.Subtitle>
         <Card.Text>{description}</Card.Text>
+        <Button
+          onClick={() => updateImage({ ...image, likes: image.likes + 1 })}
+        >
+          Like
+        </Button>
+        {"   "}
+        <Button
+          onClick={() => updateImage({ ...image, likes: image.likes - 1 })}
+        >
+          Dislike
+        </Button>
+        {"   "}
         <Link to={`/images/${_id}`}>More...</Link>
       </Card.Body>
     </Card>
